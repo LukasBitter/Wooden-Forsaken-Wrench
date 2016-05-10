d281025b05df841f6c8785da2e44620715bea059
arkeine <rytenil@gmail.com>
2016-05-10 16:27:20 +0200

    add a visual demo for svm helloworld

---
+++ demo/SVM helloword/start.cmd
@@ -0,0 +1,3 @@
+python ..\..\tests\testSVM.py 
+
+pause
--- tests/testSVM.py
+++ tests/testSVM.py
@@ -24,14 +24,25 @@ class SVM(StatModel):
         return np.float32( [self.model.predict(s) for s in samples])
 
 
+print "training with rectangle"
+print ""
+print "1 _______________1"
+print " |               |"
+print " |               | category 0"
+print " |_______________|"
+print " |               |"
+print " |               | category 1"
+print " |_______________|"
+print "0               1"
+print ""
 samples = np.array([(0.0, 0.0),(0.0, 1.0),(1.0, 1.0),(1.0,0.0)], dtype = np.float32)
 y_train = np.array([1.,0.,0.,1.], dtype = np.float32)
 y_predict = np.array([(0.2, 0.3),(1.0, 0.8)], dtype = np.float32)
+print "SVM, where is (0.2, 0.3) ? and  (1.0, 0.8) ?"
 
 clf = SVM()
 clf.train(samples, y_train)
 y_val = clf.predict(y_predict)
 
-print samples
-print y_train
-print y_val
+for i in range(0, len(y_predict)):
+    print y_predict[i], " is in category ", y_val[i]
